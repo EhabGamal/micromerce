4d890c4d02a59c57dc6fd7ad8c2418fb7a071b89
Ehab Gamal <ehab.g.sh@gmail.com>
2018-05-08 13:22:11 +0200

    Add API service routes

---
+++ front-end/app/routes.js
@@ -0,0 +1,27 @@
+const routes = [{
+  prefix: '/api',
+  pin: 'role:api,cmd:*',
+  map: {
+    products: {
+      GET: true,
+      suffix: '/:id?'
+    },
+    get: {
+      GET: true
+    }
+  }
+},{
+  prefix: '/api/order',
+  pin: 'role:api,cmd:order,action:*',
+  map: {
+    create: {
+      POST: true
+    },
+    fetch: {
+      GET: true,
+      suffix: '/:id?'
+    }
+  }
+}]
+
+module.exports = routes;
