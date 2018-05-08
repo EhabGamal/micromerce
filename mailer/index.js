349e2e1a6f71a0f28e183208bf98303cc252a01c
Ehab Gamal <ehab.g.sh@gmail.com>
2018-05-08 13:09:10 +0200

    Config service port and changed nodemailer service to gmail

--- mailer/index.js
+++ mailer/index.js
@@ -3,13 +3,6 @@ const plugin = require('./app/plugin');
 
 seneca
   .use(plugin)
-  .act({
-    role: 'email',
-    cmd: 'send',
-    to: 'Ehab Gamal <ehab.g.sh@gmail.com>',
-    subject: 'Micromerce Nodemailer Message',
-    body: '<h1>Hello from Micromerce!</h1>'
-  }, (err, res) => {
-    if(err) console.log('Error sending the email');
-    else console.log(res);
+  .ready(function(err){
+    seneca.listen(9003);
   });
