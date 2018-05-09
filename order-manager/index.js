fd3d00c70e95bb569750f379b9148dc6fefe4932
Ehab Gamal <ehab.g.sh@gmail.com>
2018-05-09 09:33:00 +0200

    Fix eslint issues in order-manager service

--- order-manager/index.js
+++ order-manager/index.js
@@ -1,10 +1,14 @@
 const seneca = require('seneca')();
-const plugin = require('./app/plugin');
+const Plugin = require('./app/plugin');
 
 seneca
   .use('entity')
   .use('mongo-store', {uri: 'mongodb://127.0.0.1:27017/micromerce'})
-  .use(plugin)
-  .ready(function(err){
+  .use(Plugin)
+  .ready((err) => {
+    if (err) {
+      seneca.log.err('Error starting order-manager service!');
+      return;
+    }
     seneca.listen(9002);
   });
